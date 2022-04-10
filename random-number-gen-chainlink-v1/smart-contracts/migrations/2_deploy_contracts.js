d7f1433b117d1272715d94d9ff7affd560189f2b
netanmangal <imnetanmangal@gmail.com>
2022-04-10 23:06:53 +0530

    Delete: Deleted truffle projects

--- random-number-gen-chainlink-v1/smart-contracts/migrations/2_deploy_contracts.js
+++
@@ -1,5 +0,0 @@
-const RandomNumberConsumer = artifacts.require("RandomNumberConsumer");
-
-module.exports = (deployer) => {
-    deployer.deploy(RandomNumberConsumer, "0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9", "0xa36085F69e2889c224210F603D836748e7dC0088", "0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4");
-}
