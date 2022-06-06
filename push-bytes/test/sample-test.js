15ad2786f32669b9d6a0f64adbe50f6daab5d731
netanmangal <imnetanmangal@gmail.com>
2022-06-06 17:26:02 +0530

    Add: Tests

--- push-bytes/test/sample-test.js
+++ push-bytes/test/sample-test.js
@@ -1,19 +1,53 @@
 const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const { web3 } = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+const { abi, bytecode } = require("../artifacts/contracts/PushBytes.sol/PushBytes.json");
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+describe("PushBytes", function () {
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+  let accounts;
+  let contractInstance;
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+  beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+    contractInstance = await new web3.eth.Contract(abi)
+      .deploy({
+        data: bytecode
+      })
+      .send({ from: accounts[0] });
   });
+
+  context("Push and get bytes", () => {
+    it("Initially, Null bytes at key 1", async () => {
+      const response = await contractInstance.methods.hash("1").call();
+      expect(response).to.equal(null);
+    });
+
+    it("Push data", async () => {
+      const value = "0x0010fe";
+
+      await contractInstance.methods.pushData("1", value).send({
+        from: accounts[0]
+      });
+      const response = await contractInstance.methods.hash("1").call();
+      expect(response).to.equal(value);
+    });
+  });
+
+  context("Access Control", () => {
+    it("Push by non-admin account : Report Error", async () => {
+      try {
+        await contractInstance.methods.pushData("1", value).send({
+          from: accounts[1]
+        });
+
+        // should throw error and control goto catch block
+
+        expect(true).to.equal(false);
+      } catch (e) {
+        expect(e).to.not.equal(null);
+      }
+    });
+  });
+
 });
