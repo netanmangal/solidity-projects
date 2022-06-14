046a879056fcfc754b92ea41708e63aee831dd79
netanmangal <imnetanmangal@gmail.com>
2022-06-14 15:14:31 +0530

    Add: dotenv + privatekey + alchemyURLS

--- push-bytes/smart-contract/hardhat.config.js
+++ push-bytes/smart-contract/hardhat.config.js
@@ -1,5 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-web3");
+require("dotenv").config();
+
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -19,4 +21,17 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.14",
+  networks: {
+    kovan: {
+      url: process.env.HTTPS_KOVAN_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    arbitrum_testnet: {
+      url: process.env.HTTPS_ARBITRUM_RINKEBY_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  mocha: {
+    timeout: 400000
+  }
 };
