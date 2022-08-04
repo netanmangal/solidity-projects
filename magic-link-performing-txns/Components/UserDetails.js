1c3d59287061717c348a9b4d7f21e002ea27cf5b
netanmangal <imnetanmangal@gmail.com>
2022-08-04 22:27:41 +0530

    Add: transfer tokens

--- magic-link-performing-txns/Components/UserDetails.js
+++ magic-link-performing-txns/Components/UserDetails.js
@@ -5,7 +5,9 @@ import { useEffect, useState } from 'react';
 const UserDetails = ({ state, setState }) => {
 
     const [localState, setLocalState] = useState({
-        userBalance: 0
+        userBalance: 0,
+        toAddr: "",
+        amount: ""
     });
 
     useEffect(() => {
@@ -23,31 +25,51 @@ const UserDetails = ({ state, setState }) => {
     }, []);
 
     return (
-        <div className={detailStyles.bigCard}>
-            <h4>Welcome To LoginScreen</h4>
-            <p className={detailStyles.subHeader}>Details:</p>
+        <>
+            <div className={detailStyles.bigCard} style={{ marginBottom: "30px" }}>
+                <h4>Welcome To LoginScreen</h4>
+                <p className={detailStyles.subHeader}>Details:</p>
 
-            <div style={{ "marginLeft": "50px" }}>
-                <p><b>Email</b>: {state.userDetails.email}</p>
-                <p><b>IsMfaEnabled</b>: {state.userDetails.isMfaEnabled}</p>
-                <p><b>Issuer</b>: {state.userDetails.issuer}</p>
-                <p><b>PhoneNumber</b>: {state.userDetails.phoneNumber}</p>
-                <p><b>PublicAddress</b>: {state.userDetails.publicAddress}</p>
-                <p><b>Token Address</b>: {state.erc20Contract.options.address}</p>
-                <p><b>User Balance</b>: {localState.userBalance}</p>
-            </div>
-
-            <div className={detailStyles.fotter}>
-                <button className={styles.gradientButton} onClick={() => {
-                    console.log("Transfer");
-                }}>
-                    Transfer
-                </button>
-                <button className={styles.gradientButton} onClick={(e) => logoutHandler(e, state, setState)}>
+                <button className={detailStyles.purpleButton} onClick={(e) => logoutHandler(e, state, setState)}>
                     LogOut
                 </button>
+
+                <div style={{ "marginLeft": "50px" }}>
+                    <p><b>Email</b>: {state.userDetails.email}</p>
+                    <p><b>IsMfaEnabled</b>: {state.userDetails.isMfaEnabled}</p>
+                    <p><b>Issuer</b>: {state.userDetails.issuer}</p>
+                    <p><b>PhoneNumber</b>: {state.userDetails.phoneNumber}</p>
+                    <p><b>PublicAddress</b>: {state.userDetails.publicAddress}</p>
+                    <p><b>Token Address</b>: {state.erc20Contract.options.address}</p>
+                    <p><b>User Balance</b>: {localState.userBalance}</p>
+                </div>
             </div>
-        </div>
+
+            <div className={detailStyles.bigCard}>
+                <p className={detailStyles.subHeader}>Transfer tokens:</p>
+                <input
+                    type={"string"}
+                    className={styles.input}
+                    placeholder="To Address"
+                    value={localState.toAddr}
+                    onChange={(e) => {
+                        setLocalState({ ...localState, toAddr: e.target.value });
+                    }}
+                />
+                <input
+                    type={"string"}
+                    className={styles.input}
+                    placeholder="Amount (in WETH)"
+                    value={localState.amount}
+                    onChange={(e) => {
+                        setLocalState({ ...localState, amount: e.target.value });
+                    }}
+                />
+                <button className={styles.gradientButton} onClick={(e) => transferHandler(e, state, setState, localState)}>
+                    Transfer
+                </button>
+            </div>
+        </>
     );
 }
 
@@ -58,6 +80,24 @@ const logoutHandler = async (e, state, setState) => {
     });
 
     await state.magic.user.logout();
+
+    setState({
+        ...state,
+        loading: false
+    });
+
+    window.location.reload();
+}
+
+const transferHandler = async (e, state, setState, localState) => {
+    setState({
+        ...state,
+        loading: true
+    });
+
+    await state.erc20Contract.methods.transfer(localState.toAddr, localState.amount).send({
+        from: state.userDetails.publicAddress
+    });
 
     setState({
         ...state,
