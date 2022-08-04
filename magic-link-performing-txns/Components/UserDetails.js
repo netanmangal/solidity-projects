62f4bb296833c0afe99eeb814b8de56c1a09c422
netanmangal <imnetanmangal@gmail.com>
2022-08-04 18:27:43 +0530

    Add: web3 integration + ERC20Contract Integration

--- magic-link-performing-txns/Components/UserDetails.js
+++ magic-link-performing-txns/Components/UserDetails.js
@@ -1,26 +1,48 @@
 import styles from '../styles/Home.module.css';
 import detailStyles from '../styles/Details.module.css';
+import { useEffect, useState } from 'react';
 
 const UserDetails = ({ state, setState }) => {
+
+    const [localState, setLocalState] = useState({
+        userBalance: 0
+    });
+
+    useEffect(() => {
+
+        const init = async () => {
+            const userBalance = await state.erc20Contract.methods.balanceOf(state.userDetails.publicAddress).call();
+            await setLocalState({
+                ...localState,
+                userBalance: userBalance
+            });
+        }
+
+        init();
+
+    }, []);
+
     return (
         <div className={detailStyles.bigCard}>
             <h4>Welcome To LoginScreen</h4>
             <p className={detailStyles.subHeader}>Details:</p>
 
-            <div style={{"marginLeft": "50px"}}>
+            <div style={{ "marginLeft": "50px" }}>
                 <p><b>Email</b>: {state.userDetails.email}</p>
                 <p><b>IsMfaEnabled</b>: {state.userDetails.isMfaEnabled}</p>
                 <p><b>Issuer</b>: {state.userDetails.issuer}</p>
                 <p><b>PhoneNumber</b>: {state.userDetails.phoneNumber}</p>
                 <p><b>PublicAddress</b>: {state.userDetails.publicAddress}</p>
+                <p><b>Token Address</b>: {state.erc20Contract.options.address}</p>
+                <p><b>User Balance</b>: {localState.userBalance}</p>
             </div>
 
             <div className={detailStyles.fotter}>
-                {/* <button className={styles.gradientButton} onClick={() => {
-                    console.log("Button pressed");
+                <button className={styles.gradientButton} onClick={() => {
+                    console.log("Transfer");
                 }}>
-                    Update
-                </button> */}
+                    Transfer
+                </button>
                 <button className={styles.gradientButton} onClick={(e) => logoutHandler(e, state, setState)}>
                     LogOut
                 </button>
--- magic-link-performing-txns/pages/index.js
+++ magic-link-performing-txns/pages/index.js
@@ -1,11 +1,14 @@
 import Head from 'next/head';
 import { Magic } from 'magic-sdk';
+import Web3 from 'web3';
 
 import styles from '../styles/Home.module.css';
 import Login from '../Components/Login';
 import Loader from '../Components/Loader';
 import UserDetails from '../Components/UserDetails';
 import { useEffect, useState } from 'react';
+import config from "../config.json";
+import erc20Data from "../utils/artifacts/ERC20Token.json";
 
 const Home = () => {
   const [state, setState] = useState({
@@ -13,18 +16,24 @@ const Home = () => {
     loading: true,
     isLogin: false,
     email: "",
-    userDetails: {}
+    userDetails: {},
+    web3: null,
+    erc20Contract: null
   });
 
   useEffect(() => {
 
     const init = async () => {
-      const magic = new Magic('pk_live_5618E4ECEC07A6F7');
+      const magic = new Magic(config.MAGIC_PUBLISHABLE_KEY, {
+        network: config.NETWORK_CHAIN
+      });
       const isLogin = await magic.user.isLoggedIn();
 
-      let userDetails = {};
+      let userDetails = {}, web3 = null, erc20Contract = null;
       if (isLogin) {
         userDetails = await magic.user.getMetadata();
+        web3 = new Web3(magic.rpcProvider)
+        erc20Contract = new web3.eth.Contract(erc20Data.abi, config.TOKEN_CONTRACT_ADDRESS)
       }
 
       await setState({
@@ -32,7 +41,9 @@ const Home = () => {
         magic: magic,
         loading: false,
         isLogin: isLogin,
-        userDetails: userDetails
+        userDetails: userDetails,
+        web3: web3,
+        erc20Contract: erc20Contract
       });
     }
 
