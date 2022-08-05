a5ed5ce814863b3ca9be9244080c170767806427
netanmangal <imnetanmangal@gmail.com>
2022-08-05 10:23:40 +0530

    Add: project init from /netanmangal/solidity-projects/magic-link

---
+++ magic-link-with-nftport/Components/Login.js
@@ -0,0 +1,50 @@
+import styles from '../styles/Home.module.css';
+import detailStyles from '../styles/Details.module.css';
+
+const Login = ({state, setState}) => {
+    return (
+        <div className={styles.card}>
+            <h4>Welcome To TheCard!</h4>
+            <p className={detailStyles.subHeader}>Login:</p>
+
+            <input
+                type={"string"}
+                className={styles.input}
+                placeholder="Email"
+                value={state.email}
+                onChange={(e) => {
+                    setState({
+                        ...state,
+                        email: e.target.value
+                    });
+                }}
+            />
+
+            <div className={detailStyles.fotter}>
+                <button className={styles.gradientButton} onClick={(e) => loginHandler(e, state, setState)}>
+                    Login
+                </button>
+            </div>
+        </div>
+    );
+}
+
+const loginHandler = async (e, state, setState) => {
+    e.preventDefault();
+    
+    setState({
+        ...state,
+        loading: true
+    });
+
+    await state.magic.auth.loginWithMagicLink({email: state.email, showUI: true});
+
+    setState({
+        ...state,
+        loading: false
+    });
+
+    window.location.reload();
+}
+
+export default Login;
