f800bc4985f84dcf611a83bf0aec1dad5e8b472a
netanmangal <imnetanmangal@gmail.com>
2022-09-07 18:24:25 +0530

    Add: hardhat config

--- upgradable-contract/hardhat.config.js
+++ upgradable-contract/hardhat.config.js
@@ -1,4 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("@openzeppelin/hardhat-upgrades");
+
+require("dotenv").config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,4 +22,12 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  defaultNetwork: "mumbai",
+  networks: {
+    mumbai: {
+      chainId: 80001,
+      url: process.env.MUMBAI_RPC_URL,
+      accounts: [`${process.env.PRIVATE_KEY}`]
+    }
+  }
 };
